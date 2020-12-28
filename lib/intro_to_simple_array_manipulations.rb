9a6c7815fa7af450fc6f3a506e1f3c9c2edec21e
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-28 22:44:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+array.push(string)
+end
+
+def using_unshift(array, string)
+ array= ["Brooklyn", "Staten Island", "Queens", "Bronx"]
+ array.unshift("Staten Island") 
+end
+
+def using_pop(array)
+array=
+array.pop
+end
+
+def pop_with_args(array)
+  array =
+  array.pop(2)
+end
